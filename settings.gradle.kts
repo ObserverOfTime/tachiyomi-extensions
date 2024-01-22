0951dab644e51b2109c4adfa838b5deabc53803e
ObserverOfTime <chronobserver@disroot.org>
2024-01-23 00:05:27 +0200

    refactor: restructure project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,14 @@
 include(":core")
 
-include(":multisrc")
-project(":multisrc").projectDir = File("multisrc")
+File(rootDir, "multisrc").eachDir { include(":multisrc:${it.name}") }
 
-File(rootDir, "lib").eachDir {
-    include(":lib:${it.name}")
+File(rootDir, "lib").eachDir { include(":lib:${it.name}") }
+
+File(rootDir, "extensions").eachDir { dir ->
+    dir.eachDir { include(":extensions:${dir.name}:${it.name}") }
 }
 
-File(rootDir, "src").eachDir { dir ->
-    dir.eachDir { subdir ->
-        val name = ":extensions:individual:${dir.name}:${subdir.name}"
-        include(name)
-        project(name).projectDir = File("src/${dir.name}/${subdir.name}")
-    }
-}
-
-File(rootDir, "generated-src").eachDir { dir ->
-    dir.eachDir { subdir ->
-        val name = ":extensions:multisrc:${dir.name}:${subdir.name}"
-        include(name)
-        project(name).projectDir = File("generated-src/${dir.name}/${subdir.name}")
-    }
-}
+rootProject.name = "tachiyomi-extensions"
 
 fun File.eachDir(block: (File) -> Unit) =
-    listFiles()?.filter { it.isDirectory }?.forEach(block)
+    listFiles(File::isDirectory)?.forEach(block)
